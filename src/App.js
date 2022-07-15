f7229872d34e95d1da064fae21939497d008a346
Abhishek-Kumar <abhishekphd14@gmail.com>
2022-07-15 13:02:48 +0530

    Backup frontend code for edudron wesbite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import "./PageCss/App.css";
+import "./PageCss/navbar.css";
+import "./PageCss/Home.css";
+import "./PageCss/footer.css";
+import "./PageCss/subscription.css";
+import Navbar from "./components/Navbar";
+// import ContactUs from "./components/ContactUs";
+import Login from "./loginpage/Login";
+import Signup from "./loginpage/Signup";
+import DemoCreater from "./components/DemoCreater";
+import Home from "./Home";
+import Footer from "./components/Footer";
+
+// import AboutUs from './components/AboutUs'; // <------ {CSS issues}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+    <Navbar/>
+      
+       <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/contact" element={<Signup />} />
+        <Route path="/creater" element={<DemoCreater />} />
+        {/* <Route path="/about" element={<AboutUs/>} /> */}
+        {/* <Route path="/contactus" element={<ContactUs/>} /> */}
+         <Route
+          path="*"
+          element={
+            <div>
+              <h2>404 Page not found etc.. </h2>
+            </div>
+          }
+        />
+      </Routes>  
+      <Footer/>
+    </>
   );
 }
 
